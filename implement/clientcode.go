a9617c62feb327d0985d66ebbbb5003e11e5f391
goen <fuuucl.am@gmail.com>
2014-11-19 21:58:51 +0100

    Client's code generates some sample data

--- implement/clientcode.go
+++ implement/clientcode.go
@@ -11,28 +11,36 @@ type my struct {
 	age int
 }
 
-func (seed *uint64) generate() (out my) {
+type seed uint64
+
+func (o *seed) gen() (out my) {
 	for j := 0; j < 3; j++ {
 		for i := range(out.species) {
-			o *= 16777619
-			out.species[i] ^= o
+			(*o) *= 16777619
+			out.species[i] ^= byte(*o)
 		}
 		for i := range(out.name) {
-			o *= 16777619
-			out.name[i] ^= o
+			(*o) *= 16777619
+			out.name[i] ^= byte(*o)
 		}
 		for i := range(out.surname) {
-			o *= 16777619
-			out.surname[i] ^= o
+			(*o) *= 16777619
+			out.surname[i] ^= byte(*o)
 		}
-		o *= 16777619
-		out.age[i] ^= o
+		(*o) *= 16777619
+		out.age ^= int(*o)
 	}
+	return
 }
 
 
-func () {
-	programmers []my
+func main() {
+	var o seed = 2166136261
+	var programmers []my
 
+	for i := 0; i < 100000; i++ {
+		programmers = append(programmers, (&o).gen())
+	}
 
+	fmt.Println(programmers[100])
 }
